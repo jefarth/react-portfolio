050c9bcc2346fce6e95e647ff348eb97ac6f281f
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-03-26 13:10:08 -0600

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { Grid } from '@mui/material'
-
+import Header from './components/Header';
 
 function App() {
   return (
     <>
     <Grid>
-
+      <Header />
     </Grid>
     </>
   );
