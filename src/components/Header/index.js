e2a53226bd3e5fa9820cfb5c86d8d61f6ba018b2
unknown <jeremiah.farthing@gmail.com>
2022-04-14 13:02:56 -0600

    Updated icon buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,22 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
+        <Toolbar sx={{ mx: 2 }}>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Jeremiah Farthing
           </Typography>
@@ -28,6 +20,24 @@ export default function Header() {
           <Button color="inherit">.aboutMe()</Button>
           <Button color="inherit">.projects()</Button>
           <Button color="inherit">.contactMe()</Button>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="linkedin"
+            sx={{ mr: 2 }}
+          >
+            <LinkedInIcon />
+          </IconButton>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="github"
+            sx={{ mr: 2 }}
+          >
+            <GitHubIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
