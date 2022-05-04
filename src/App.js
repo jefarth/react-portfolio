a29bea1ef77c597588f7180abe6f2367262b2a26
unknown <jeremiah.farthing@gmail.com>
2022-05-03 19:29:34 -0600

    created basic home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import { Grid } from '@mui/material'
+import { Grid } from '@mui/material';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
     <Grid>
       <Header />
+      <Home />
     </Grid>
     </>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material';
+
+export const Home = () => {
+    return (
+        <Grid>
+            <Grid>
+                <h1 xs={12}>Jeremiah Farthing</h1>
+                <h2 xs={12}>Web Developer</h2>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Home;
